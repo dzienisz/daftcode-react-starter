b5cd69483b152cd85455dc536d0b0c7e5150df6c
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2018-04-23 11:59:47 +0200

    Counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { hot } from 'react-hot-loader';
 import * as React from 'react';
-import Home from './view/Home';
 
 import './styles/theme.sass';
+import Counter from './view/Counter';
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <main>
-        <Home username="DaftCoder" />
+        <Counter from={ 68 } to={ 0 } />
       </main>
     );
   }
