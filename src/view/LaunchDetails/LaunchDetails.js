1173dc85783354aed33dad069ac1b4994fb859f3
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2018-05-09 14:57:02 +0200

    editor fixes 🎈

--- src/view/LaunchDetails/LaunchDetails.js
+++ src/view/LaunchDetails/LaunchDetails.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import Header from '../../components/Header/Header';
 
 class LaunchDetails extends Component {
   render() {
     return (
       <div>
-          <h1>SpaceX - DaftCode Level Up Spring 2018</h1>
+        <h1>SpaceX - DaftCode Level Up Spring 2018</h1>
+        <Header />
       </div>
     );
   }
